bf6ed71b07defc8fc674cf623bb4927ca6d5df4d
marcosiraola <iraolamarcos@gmail.com>
2022-07-05 21:16:47 -0300

    responsive

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,7 +4,7 @@ import Card from '../../components/card/card';
 function Home () {
 
     return (
-        <div className="container">
+        <div className="home-container">
             <Card />
         </div>
     );
